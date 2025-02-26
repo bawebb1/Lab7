c0febabb8f95e42eeb2e7b17cd46a84469c41366
bawebb1 <vyw851@mocs.utc.edu>
2025-02-26 15:57:22 -0500

    Update Python.py

--- Python.py
+++ Python.py
@@ -1,9 +1,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_absolute_error
 
 df = pd.read_excel('AmesHousing.xlsx')
 
